fd8234656a0aafdd2c868c5652aecddbbe5e2d9b
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-07-09 21:11:48 +0200

    created models for ensamble nn and ensamble RF

---
+++ example/comparisons/models.py
@@ -0,0 +1,59 @@
+from sklearn.neural_network import MLPRegressor
+import random 
+import numpy as np
+class Ensamble_NN():
+    def __init__ (self, n_estimators, hidden_layer_sizes=(100, 100, 100,100)):
+        self.ensemble = [MLPRegressor(hidden_layer_sizes=hidden_layer_sizes, random_state=random.randint(0, 2**32 - 1)) for _ in range(n_estimators)]
+    
+    def fit(self, X, y):
+        # Train the ensemble of neural networks
+        for nn in self.ensemble:
+            nn.fit(X, y)
+
+    def predict(self, X, return_std=False):
+        predictions = [nn.predict(X) for nn in self.ensemble]
+        predictions = np.array(predictions)  # Shape: (n_estimators, n_samples)
+
+        # Calculate the mean and variance of the predictions
+        mean = np.mean(predictions, axis=0)
+        if return_std:
+            # variance_predictions = np.var(predictions, axis=0)
+            # print("Predection shape", predictions.shape)
+            std= np.std(predictions, axis=0)
+            # print(std)
+            return mean, std
+        return mean
+    
+    def samples(self, X):
+        predictions = [nn.predict(X) for nn in self.ensemble]
+        predictions = np.array(predictions)  # Shape: (n_estimators, n_samples)
+        return predictions
+    
+from sklearn.ensemble import RandomForestRegressor
+
+class Ensemble_RF():
+    def __init__(self, n_estimators, max_depth):
+        self.n_estimators = n_estimators
+        self.ensemble = [RandomForestRegressor(random_state=random.randint(0, 2**32 - 1), max_depth=max_depth) for _ in range(n_estimators)]
+
+    def fit(self, X, y):
+        # Train the ensemble of random forests
+        for rf in self.ensemble:
+            rf.fit(X, y)
+
+    def predict(self, X, return_std=False):
+        predictions = [rf.predict(X) for rf in self.ensemble]
+        predictions = np.array(predictions)  # Shape: (n_estimators, n_samples)
+
+        # Calculate the mean and variance of the predictions
+        mean = np.mean(predictions, axis=0)
+        if return_std:
+            variance_predictions = np.var(predictions, axis=0)
+            std = np.sqrt(variance_predictions)
+            return mean, std
+        return mean
+    
+    def samples(self, X):
+        predictions = [nn.predict(X) for nn in self.ensemble]
+        predictions = np.array(predictions)  # Shape: (n_estimators, n_samples)
+        return predictions
