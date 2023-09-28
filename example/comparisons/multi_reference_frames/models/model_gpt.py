6293853d28ef3b624265227b1b01f8d00819dea1
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-09-28 20:30:13 +0200

    added also the model of multiple reference frames using a gp transportation

--- example/comparisons/multi_reference_frames/models/model_gpt.py
+++ example/comparisons/multi_reference_frames/models/model_gpt.py
@@ -13,56 +13,98 @@ class Multiple_Reference_Frames_GPT:
     def __init__(self):
         pass
 
+    def load_dataset(self, filename = 'reach_target'):
+        
 
-    def load_data(self):
-        self.distribution=None
+        demos = np.load(filename + '.npy', allow_pickle=True, encoding='latin1')[()]
+
+        ### Trajectory data
+        demos_x = demos['x'] # position
+
+        ### Coordinate systems transformation
+        demos_A = [d for d in demos['A']]
+        demos_b = [d for d in demos['b']]
+
+        distribution=np.zeros((len(demos_x),10,2))
+        final_distance=np.zeros((len(demos_x),2))
+        final_orientation=np.zeros((len(demos_x),1))
+        # index=2
+        frame_dim=5
+        for i in range(len(demos_x)):
+            distribution[i,0,:]=demos_b[i][0][0]
+            distribution[i,1,:]=demos_b[i][0][0]+demos_A[i][0][0] @ np.array([ 0, frame_dim])
+            distribution[i,2,:]=demos_b[i][0][1]
+            distribution[i,3,:]=demos_b[i][0][1]+demos_A[i][0][1] @ np.array([ 0, -frame_dim])
+            #Extra points
+            distribution[i,4,:]=demos_b[i][0][0]+demos_A[i][0][0] @ np.array([ 0, -frame_dim])
+            distribution[i,5,:]=demos_b[i][0][1]+demos_A[i][0][1] @ np.array([ 0, frame_dim])
+
+            distribution[i,6,:]=demos_b[i][0][0]+demos_A[i][0][0] @ np.array([ +frame_dim, 0])
+            distribution[i,7,:]=demos_b[i][0][1]+demos_A[i][0][1] @ np.array([ +frame_dim, 0])
+            distribution[i,8,:]=demos_b[i][0][0]+demos_A[i][0][0] @ np.array([ -frame_dim, 0])
+            distribution[i,9,:]=demos_b[i][0][1]+demos_A[i][0][1] @ np.array([ -frame_dim, 0])
+
+
+            final_distance[i]=  np.linalg.inv(demos_A[i][0][1]) @ (demos_x[i][-1,:] - demos_b[i][0][1])
+
+            final_delta=np.linalg.inv(demos_A[i][0][1]) @ (demos_x[i][-1,:]-demos_x[i][-2,:])
+            final_orientation[i]= np.arctan2(final_delta[1],final_delta[0])
+
+        self.demos_x=demos_x
+        self.demos_A=demos_A
+        self.demos_b=demos_b
+        self.distribution=distribution
+        self.final_distance=final_distance
+        self.final_orientation=final_orientation    
 
     def train(self):
         self.transport=Transport()
         k_transport = C(constant_value=np.sqrt(10))  * Matern(20*np.ones(1), [10,50], nu=2.5) + WhiteKernel(0.01 , [0.0000001, 0.000001])
         self.transport.kernel_transport=k_transport
-    def reproduce(self, index_source, ax=None, compute_metrics=False, plot=False):
+
+
+    def reproduce(self, index_source, index_target, ax=None, compute_metrics=False):
         X=self.demos_x[index_source]
 
-        self.transport.source_distribution=distribution[index_source,:,:]
-        self.transport.target_distribution=distribution_input[index_target,:,:]
+        self.transport.source_distribution=self.distribution[index_source,:,:]
+        self.transport.target_distribution=self.distribution[index_target,:,:]
         self.transport.training_traj=X
         self.transport.fit_transportation()
         self.transport.apply_transportation()
         X1=self.transport.training_traj
         std=self.transport.std
 
-        if plot==True:
+        if ax is not None:
             draw_error_band(ax, X1[:,0], X1[:,1], err=std, facecolor= [255.0/256.0,140.0/256.0,0.0], edgecolor="none", alpha=.8)
-            ax.plot(transport.target_distribution[0:2,0],transport.target_distribution[0:2,1], linewidth=10, alpha=0.9, c='green')
-            ax.scatter(transport.target_distribution[0,0],transport.target_distribution[0,1], linewidth=10, alpha=0.9, c='green')
-            ax.plot(transport.target_distribution[2:4,0],transport.target_distribution[2:4,1], linewidth=10, alpha=0.9, c= [30.0/256.0,144.0/256.0,255.0/256.0])
-            ax.scatter(transport.target_distribution[2,0],transport.target_distribution[2,1], linewidth=10, alpha=0.9, c= [30.0/256.0,144.0/256.0,255.0/256.0])
-            ax.plot(transport.target_distribution[:,0],transport.target_distribution[:,1], 'b*',  linewidth=0.2)
+            ax.plot(self.transport.target_distribution[0:2,0],self.transport.target_distribution[0:2,1], linewidth=10, alpha=0.9, c='green')
+            ax.scatter(self.transport.target_distribution[0,0],self.transport.target_distribution[0,1], linewidth=10, alpha=0.9, c='green')
+            ax.plot(self.transport.target_distribution[2:4,0],self.transport.target_distribution[2:4,1], linewidth=10, alpha=0.9, c= [30.0/256.0,144.0/256.0,255.0/256.0])
+            ax.scatter(self.transport.target_distribution[2,0],self.transport.target_distribution[2,1], linewidth=10, alpha=0.9, c= [30.0/256.0,144.0/256.0,255.0/256.0])
+            ax.plot(self.transport.target_distribution[:,0],self.transport.target_distribution[:,1], 'b*',  linewidth=0.2)
             ax.plot(X1[:,0],X1[:,1], c= [255.0/256.0,20.0/256.0,147.0/256.0])
-            if training_set==True:
-                ax.plot(demos_x[index_target][:,0],demos_x[index_target][:,1], 'b--')
+            ax.plot(self.demos_x[index_target][:,0],self.demos_x[index_target][:,1], 'b--')
+
+        if compute_metrics==True:    
         # Discrete Frechet distance
-        if training_set==True:
-            df = similaritymeasures.frechet_dist(demos_x[index_target], X1)
+            df = similaritymeasures.frechet_dist(self.demos_x[index_target], X1)
 
             # quantify the difference between the two curves using
             # area between two curves
-            area = similaritymeasures.area_between_two_curves(demos_x[index_target], X1)
+            area = similaritymeasures.area_between_two_curves(self.demos_x[index_target], X1)
 
             # quantify the difference between the two curves using
             # Dynamic Time Warping distance
-            dtw, d = similaritymeasures.dtw(demos_x[index_target], X1)
+            dtw, d = similaritymeasures.dtw(self.demos_x[index_target], X1)
 
             # fde=np.linalg.norm(demos_x[i][-1]-X1[-1])
-            fd=  np.linalg.inv(demos_A[index_target][0][1]) @ (X1[-1] - demos_b[index_target][0][1])
-            fde=np.linalg.norm(final_distance[index_target]-fd)
+            fd=  np.linalg.inv(self.demos_A[index_target][0][1]) @ (X1[-1] - self.demos_b[index_target][0][1])
+            fde=np.linalg.norm(self.final_distance[index_target]-fd)
 
-            final_vel=  np.linalg.inv(demos_A[index_target][0][1]) @ (X1[-1] - X1[-5])
+            final_vel=  np.linalg.inv(self.demos_A[index_target][0][1]) @ (X1[-1] - X1[-5])
 
             final_angle= np.arctan2(final_vel[1], final_vel[0])
 
-            final_angle_distance= np.abs(final_angle - final_orientation[i])  
+            final_angle_distance= np.abs(final_angle - self.final_orientation[index_target])  
 
             print("Final Point Distance  : ", fde)
             print("Frechet Distance      : ", df)
