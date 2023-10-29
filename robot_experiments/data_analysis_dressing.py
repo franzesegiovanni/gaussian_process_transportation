3ba2a50d7c1983d1599a69d550ae4104236e2d6b
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-10-29 18:07:33 +0100

    printed the arm in the plot of the dressing experimetns

--- robot_experiments/data_analysis_dressing.py
+++ robot_experiments/data_analysis_dressing.py
@@ -1,12 +1,11 @@
 import os
 import pickle 
-import sys
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 #load source distribution
 file_dir= os.path.dirname(__file__)
 from convert_tag_2_dist import convert_distribution
-
+import numpy as np
 data_folder =file_dir+ "/results/dressing/"
 target = []
 traj = []
@@ -24,6 +23,23 @@ traj_demo_ori= load(file_dir+ "/results/dressing/traj_demo_ori.pkl")
 
 source_tags = load(data_folder + "/source.pkl")
 
+import itertools
+def sort_points(points):
+    permutations = list(itertools.permutations(points))
+    cost = []
+    for perm in permutations:
+        perm= np.array(perm)
+        cost.append(np.sum(np.linalg.norm(perm[1:,:]-perm[:-1,:],1)))
+    min_index = np.argmin(cost)
+    permuted_list = permutations[min_index]
+    index = []
+    # print(points)
+    for i in range(len(points)):
+        index.append(int(np.argmin(np.linalg.norm(permuted_list-points[i], axis=1))))
+    # index = [int(x) for x in index]  
+    print(index)  
+    return permuted_list, index
+
 
 for filename in sorted(os.listdir(data_folder)):
     if filename.endswith(".pkl") and not "demo" in filename:
@@ -40,30 +56,33 @@ for filename in sorted(os.listdir(data_folder)):
             name_traj.append(os.path.splitext(filename)[0])
 
 
-print(name_target)
-print(name_traj)
-print(name_traj_ori)
-
 plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-ax.plot(traj_demo[:,0], traj_demo[:,1], traj_demo[:,2], '--')
-ax.scatter(traj_demo[0,0], traj_demo[0,1], traj_demo[0,2], c='r', marker='o')
-ax.scatter(traj_demo[-1,0], traj_demo[-1,1], traj_demo[-1,2], c='g', marker='o')
-ax.scatter(source_dist[:,0], source_dist[:,1], source_dist[:,2], c='b', marker='o')
+ax.plot(traj_demo[:,0], traj_demo[:,1], traj_demo[:,2], '--', label='demo')
+ax.scatter(traj_demo[0,0], traj_demo[0,1], traj_demo[0,2], c='r', marker='o', label='start')
+ax.scatter(traj_demo[-1,0], traj_demo[-1,1], traj_demo[-1,2], c='g', marker='o', label='end')
+source_dist , index = sort_points(source_dist)
+source_dist = np.vstack(source_dist)
+ax.plot(source_dist[:,0], source_dist[:,1], source_dist[:,2], c='b', marker='o')
+
 
 
 for i, trajectory in enumerate(traj):
-    plt.figure()
-    ax = plt.axes(projection='3d')
+    # plt.figure()
+    # ax = plt.axes(projection='3d')
     ax.plot(trajectory[:,0], trajectory[:,1], trajectory[:,2])
-    ax.scatter(trajectory[0,0], trajectory[0,1], trajectory[0,2], c='r', marker='o', label='start')
-    ax.scatter(trajectory[-1,0], trajectory[-1,1], trajectory[-1,2], c='g', marker='o', label='end')
-    ax.scatter(target[i][:,0], target[i][:,1], target[i][:,2], c='b', marker='o')
+    ax.scatter(trajectory[0,0], trajectory[0,1], trajectory[0,2], c='r', marker='o')
+    ax.scatter(trajectory[-1,0], trajectory[-1,1], trajectory[-1,2], c='g', marker='o')
+    selected_target =np.array( target[i])
+    target_sorted = selected_target[index,:]
+    ax.plot(target_sorted[:,0], target_sorted[:,1], target_sorted[:,2], c='k', marker='o')
+    ax.scatter(target_sorted[0,0], target_sorted[0,1], target_sorted[0,2], c='g', marker='o')
+    ax.scatter(target_sorted[1,0], target_sorted[1,1], target_sorted[1,2], c='b', marker='o')
+    ax.scatter(target_sorted[2,0], target_sorted[2,1], target_sorted[2,2], c='r', marker='o')
+    ax.scatter(target_sorted[3,0], target_sorted[3,1], target_sorted[3,2], c='k', marker='o')
 
 plt.legend()
 plt.show()
-
-
