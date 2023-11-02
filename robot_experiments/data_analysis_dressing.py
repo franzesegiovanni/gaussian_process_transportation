121080372f62dc0ceee91ee0a5aca82c35be60a6
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-11-02 19:29:05 +0100

    computed some numbers from the data of the dressing

--- robot_experiments/data_analysis_dressing.py
+++ robot_experiments/data_analysis_dressing.py
@@ -2,7 +2,7 @@ import os
 import pickle 
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-from utils import sort_points
+from utils import sort_points, calculate_elbow_angle
 #load source distribution
 file_dir= os.path.dirname(__file__)
 from convert_tag_2_dist import convert_distribution
@@ -61,6 +61,7 @@ ax.set_ylim([-0.3, 0.4])
 ax.set_zlim([0, 0.6])
 ax.text(x=0.1, y=0.4, z=0.6, s='Demo', fontsize=20, ha='left', va='top')
 
+print("Elbow angle: ", calculate_elbow_angle(source_dist[0,:], source_dist[1,:], source_dist[2,:]))
 # Set the point of view
 ax.view_init(elev=40, azim=100)
 
@@ -83,7 +84,8 @@ ax.grid(False)
 
 fig,axs = plt.subplots(2,5,subplot_kw={'projection': '3d'},figsize=(10, 6))
 
-# traj.pop(0)
+shoulder_pos = []
+albow_angle = []
 for i, trajectory in enumerate(traj):
     # plt.figure()
     # ax = plt.axes(projection='3d')
@@ -98,6 +100,11 @@ for i, trajectory in enumerate(traj):
     ax.scatter(target_sorted[1,0], target_sorted[1,1], target_sorted[1,2], c='y', marker='o')
     ax.scatter(target_sorted[2,0], target_sorted[2,1], target_sorted[2,2], c='m', marker='o')
     ax.scatter(target_sorted[3,0], target_sorted[3,1], target_sorted[3,2], c='c', marker='o')
+    angle_elbow=calculate_elbow_angle(target_sorted[3,:], target_sorted[2,:], target_sorted[1,:])
+    shoulder_pos.append(target_sorted[3,:])
+    albow_angle.append(angle_elbow)
+    # print("Shoulder position: ", target_sorted[3,:])    
+
     ax.set_xlim([0.1, 0.7])
     ax.set_ylim([-0.3, 0.4])
     ax.set_zlim([0, 0.6])
@@ -105,6 +112,8 @@ for i, trajectory in enumerate(traj):
         ax.text(x=0.2, y=0.4, z=0.6, s='D', fontsize=20, ha='left', va='top')
     else:
         ax.text(x=0.1, y=0.4, z=0.6, s=str(i), fontsize=20, ha='left', va='top')
+
+    ax.text(x=0.7, y=-0.3, z=0.6, s='\u03B1 ='+str(int(angle_elbow))+'\u00b0', fontsize=10, ha='left', va='top')    
 
     # Set the point of view
     ax.view_init(elev=40, azim=100)
@@ -125,5 +134,20 @@ plt.subplots_adjust(wspace=-0.1, hspace=-0.6)
 fig_folder =file_dir+ "/figures/"
 plt.savefig(fig_folder+'dressing.pdf', dpi=300, bbox_inches='tight')
 
+
+shoulder_pos=np.array(shoulder_pos)
+albow_angle=np.array(albow_angle)
+import numpy as np
+
+# your existing code
+
+shoulder_pos=np.array(shoulder_pos)
+albow_angle=np.array(albow_angle)
+# compute value range and print it
+shoulder_pos=shoulder_pos.reshape(-1,3)
+print("Shoulder position value range_x: ", np.ptp(shoulder_pos[:,0]))
+print("Shoulder position value range_y: ", np.ptp(shoulder_pos[:,1]))
+print("Shoulder position value range_z: ", np.ptp(shoulder_pos[:,2]))
+print("Elbow angle value range: ", np.ptp(albow_angle))
 # plt.legend()
 plt.show()
