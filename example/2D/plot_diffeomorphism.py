a358943a511f14dc1d603a5d7b88e37598ffd04d
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-11-08 20:14:29 +0100

    cleaned the code to generate the diffeomorphism code

--- example/2D/plot_diffeomorphism.py
+++ example/2D/plot_diffeomorphism.py
@@ -43,11 +43,11 @@ transport.kernel_transport=k_transport
 print('Transporting the dynamical system on the new surface')
 transport.fit_transportation()
 
-x_lim=[np.min(X[:,0]-10), np.max(X[:,0]+20)]
-y_lim=[np.min(X[:,1]-5), np.max(X[:,1]+30)]
+x_lim=[np.min(X[:,0]-15), np.max(X[:,0]+15)]
+y_lim=[np.min(X[:,1]-15), np.max(X[:,1]+15)]
 ## Plot diffeomorphism
 
-fig, axs = plt.subplots(1, 3, figsize=(21, 8))
+fig, axs = plt.subplots(1, 3, figsize=(14, 4))
 
 
 num_points=15
@@ -58,14 +58,19 @@ X, Y = np.meshgrid(x_grid, y_grid)
 
 grid=np.hstack((X.reshape(-1,1),Y.reshape(-1,1)))
 
-axs[0].scatter(grid[:,0],grid[:,1], color=[1,0,0])
-
+norms = np.linalg.norm(grid - np.min(grid, axis=0), axis=1)
+colors = norms[:, np.newaxis] / norms.max()  # normalize the norms to [0, 1]
+cmap='PiYG'
+axs[0].scatter(grid[:,0],grid[:,1], c=colors, cmap=cmap)
+axs[0].legend()
 for i in range(num_points):
         axs[0].plot(X[i,:],Y[i,:], color=[0,0,0])
         axs[0].plot(X[:,i],Y[:,i], color=[0,0,0])
-axs[0].scatter(source_distribution[:,0],source_distribution[:,1], color=[0,1,0])
+axs[0].scatter(source_distribution[:,0],source_distribution[:,1], color=[1,0,0], label='Source Distribution')
 axs[0].set_xlim(x_lim)
 axs[0].set_ylim(y_lim)
+axs[0].set_title('Source distribution', fontsize=20)
+axs[0].legend()
 # plt.axis('equal')
 
 transport.training_traj=grid
@@ -75,8 +80,8 @@ transport.apply_transportation_linear()
 grid_new=transport.training_traj
 X=grid_new[:,0].reshape(num_points,num_points)
 Y=grid_new[:,1].reshape(num_points,num_points)
-# plt.contourf(X, Y, cmap='viridis', levels=20) 
-axs[1].scatter(grid_new[:,0],grid_new[:,1], color=[1,0,0])
+# plt.contourf(X, Y, cmap=cmap, levels=20) 
+axs[1].scatter(grid_new[:,0],grid_new[:,1],c=colors, cmap=cmap)
 
 
 axs[1].set_xlim(x_lim)
@@ -86,19 +91,22 @@ for i in range(num_points):
         axs[1].plot(X[i,:],Y[i,:], color=[0,0,0])
         axs[1].plot(X[:,i],Y[:,i], color=[0,0,0])
 
-axs[1].scatter(target_distribution[:,0],target_distribution[:,1], color=[0,1,0])
-
-
+axs[1].scatter(target_distribution[:,0],target_distribution[:,1], color=[0,0,1], label='Target Distribution')
+axs[1].set_title('Affine Transformation', fontsize=20)
+axs[1].set_yticks([])
+axs[1].set_yticklabels([])
 transport.training_traj=grid
 # transport.training_delta=None
-
+# transport.optimize_diffeomorphism()
 transport.apply_transportation()
 grid_new=transport.training_traj
 X=grid_new[:,0].reshape(num_points,num_points)
 Y=grid_new[:,1].reshape(num_points,num_points)
-# plt.contourf(X, Y, cmap='viridis', levels=20) 
+axs[1].legend()
 
-axs[2].scatter(grid_new[:,0],grid_new[:,1], color=[1,0,0])
+# plt.contourf(X, Y, cmap=cmap, levels=20) 
+
+axs[2].scatter(grid_new[:,0],grid_new[:,1], c=colors, cmap=cmap)
 
 
 axs[2].set_xlim(x_lim)
@@ -108,6 +116,12 @@ for i in range(num_points):
         axs[2].plot(X[i,:],Y[i,:], color=[0,0,0])
         axs[2].plot(X[:,i],Y[:,i], color=[0,0,0])
 
-axs[2].scatter(target_distribution[:,0],target_distribution[:,1], color=[0,1,0])
+axs[2].scatter(target_distribution[:,0],target_distribution[:,1], color=[0,0,1], label='Target Distribution')
+axs[2].set_title(' GP Transportation', fontsize=20)
+axs[2].legend()
+axs[2].set_yticks([])
+axs[2].set_yticklabels([])
 
+plt.subplots_adjust(wspace=0.05)
+plt.savefig(source_path+'/pictures/diffeomorphism.pdf', dpi=300, bbox_inches='tight')
 plt.show()
