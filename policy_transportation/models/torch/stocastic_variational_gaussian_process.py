2a96987d79fe7b092e96e88f90b5e8a06f0733bd
giovannifranzese94 <giovanni.franzese94@gmail.com>
2024-06-15 20:40:21 +0200

    removed redundant codes from svgp

--- policy_transportation/models/torch/stocastic_variational_gaussian_process.py
+++ policy_transportation/models/torch/stocastic_variational_gaussian_process.py
@@ -9,61 +9,7 @@ from tqdm import tqdm
 import torch
 import gpytorch
 from torch.utils.data import TensorDataset, DataLoader
-from gpytorch.models import ApproximateGP
 from  torch.autograd.functional import jacobian
-# num_latents = 1
-# num_tasks = 2
-
-class SVGP_LMC(ApproximateGP):
-    def __init__(self, X, Y, num_inducing=200, num_tasks=1, num_latents=1):
-        # Let's use a different set of inducing points for each latent function
-
-        # We have to mark the CholeskyVariationalDistribution as batch
-        # so that we learn a variational distribution for each task
-        index=np.array(range(X.shape[0]))
-        sample_index=np.random.choice(index, num_inducing)
-        self.inducing_points= torch.from_numpy(X[sample_index,:]).float()
-
-        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
-            self.inducing_points.size(-2), batch_shape=torch.Size([num_latents])
-        )
-
-        # We have to wrap the VariationalStrategy in a LMCVariationalStrategy
-        # so that the output will be a MultitaskMultivariateNormal rather than a batch output
-        variational_strategy = gpytorch.variational.LMCVariationalStrategy(
-            gpytorch.variational.VariationalStrategy(
-                self, self.inducing_points, variational_distribution, learn_inducing_locations=True
-            ),
-            num_tasks=num_tasks,
-            num_latents=num_latents,
-            latent_dim=-1
-        )
-
-        super().__init__(variational_strategy)
-
-        # The mean and covariance modules should be marked as batch
-        # so we learn a different set of hyperparameters
-        self.mean_module = gpytorch.means.ZeroMean(batch_shape=torch.Size([num_latents]))
-        self.covar_module = gpytorch.kernels.ScaleKernel(
-            gpytorch.kernels.MaternKernel(batch_shape=torch.Size([num_latents])),
-            batch_shape=torch.Size([num_latents]), ard_num_dims=X.shape[1], nu=2.5
-        )
-        # self.covar_module = gpytorch.kernels.ScaleKernel(
-        #     gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_latents])),
-        #     batch_shape=torch.Size([num_latents]), ard_num_dims=X.shape[1])   
-        interval=gpytorch.constraints.Interval(0.00000001,0.000001)
-        self.likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=Y.shape[1], noise_constraint=interval)
-        self.X=torch.from_numpy(X).float()
-        self.Y=torch.from_numpy(Y).float()
-
-    def forward(self, x):
-        # The forward function should be written as if we were dealing with each output
-        # dimension in batch
-
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)       
-
 
 
 class SVGP(gpytorch.models.ApproximateGP):
@@ -92,12 +38,9 @@ class SVGP(gpytorch.models.ApproximateGP):
         # so we learn a different set of hyperparameters
         self.mean_module = gpytorch.means.ZeroMean(batch_shape=torch.Size([num_tasks]))
         self.covar_module = gpytorch.kernels.ScaleKernel(
-            gpytorch.kernels.MaternKernel(batch_shape=torch.Size([num_tasks])),
-            batch_shape=torch.Size([num_tasks]), ard_num_dims=X.shape[1], nu=2.5
-        )
-        # self.covar_module = gpytorch.kernels.ScaleKernel(
-        #     gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_latents])),
-        #     batch_shape=torch.Size([num_latents]), ard_num_dims=X.shape[1])   
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_tasks]), ard_num_dims=X.shape[1]),
+            batch_shape=torch.Size([num_tasks]))
+         
         interval=gpytorch.constraints.Interval(0.00000001,0.000001)        
         self.likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=Y.shape[1], noise_constraint=interval)
         self.X=torch.from_numpy(X).float()
@@ -115,8 +58,6 @@ class StocasticVariationalGaussianProcess():
     def __init__(self, X, Y, num_inducing=100):
         torch.cuda.empty_cache()
         self.use_cuda=torch.cuda.is_available()
-        # self.use_cuda=0
-        # self.gp= SVGP_LMC(num_latents=1, X=X, Y=Y ,num_task=Y.shape[1], num_inducing=num_inducing)
         self.gp= SVGP(X=X, Y=Y ,num_tasks=Y.shape[1], num_inducing=num_inducing)
         if self.use_cuda:
             self.gp=self.gp.cuda()
@@ -158,7 +99,7 @@ class StocasticVariationalGaussianProcess():
         x=torch.from_numpy(x).float()
         if self.use_cuda:
             x=x.cuda()
-        predictions = self.gp.likelihood(self.gp(x))
+        predictions = self.gp(x)
         if return_std:
             return predictions.mean.detach().cpu().numpy(), predictions.stddev.cpu().detach().numpy() 
         else:
