76039c1f9a705d40e56ec463e4a4a863c7e6d544
giovannifranzese94 <giovanni.franzese94@gmail.com>
2024-06-19 18:29:37 +0200

    tested surface generalization with varince minimizatiion and fixed  a bug

--- example/2D/surface_generalization.py
+++ example/2D/surface_generalization.py
@@ -12,14 +12,14 @@ import matplotlib.pyplot as plt
 from policy_transportation import GaussianProcess as GPR
 from policy_transportation import GaussianProcessTransportation as Transport
 import pathlib
-from policy_transportation.plot_utils import plot_vector_field 
+from policy_transportation.plot_utils import plot_vector_field, plot_vector_field_minvar
 from policy_transportation.utils import resample
 import warnings
 warnings.filterwarnings("ignore")
 #%% Load the drawings
 
 source_path = str(pathlib.Path(__file__).parent.absolute())  
-data =np.load(source_path+ '/data/'+str('example')+'.npz')
+data =np.load(source_path+ '/data/'+str('surface3')+'.npz')
 X=data['demo'] 
 S=data['floor'] 
 S1=data['newfloor']
@@ -39,7 +39,7 @@ gp_deltaX=GPR(kernel=k_deltaX)
 gp_deltaX.fit(X, deltaX)
 x_grid=np.linspace(np.min(X[:,0]-10), np.max(X[:,0]+10), 100)
 y_grid=np.linspace(np.min(X[:,1]-10), np.max(X[:,1]+10), 100)
-plot_vector_field(gp_deltaX, x_grid,y_grid,X,S)
+plot_vector_field_minvar(gp_deltaX, x_grid,y_grid,X,S)
 
 fig = plt.figure(figsize = (12, 7))
 plt.xlim([-50, 50-1])
@@ -59,7 +59,7 @@ transport.training_delta=deltaX
 k_transport = C(constant_value=np.sqrt(0.1))  * RBF(40*np.ones(2), length_scale_bounds=[0.01, 500]) + WhiteKernel(0.01, noise_level_bounds=[0.01, 0.1] )
 transport.kernel_transport=k_transport
 print('Transporting the dynamical system on the new surface')
-transport.fit_transportation()
+transport.fit_transportation(do_scale=False, do_rotation=True)
 transport.apply_transportation()
 X1=transport.training_traj
 deltaX1=transport.training_delta 
@@ -71,5 +71,5 @@ gp_deltaX1=GPR(kernel=k_deltaX1)
 gp_deltaX1.fit(X1, deltaX1)
 x1_grid=np.linspace(np.min(X1[:,0]-10), np.max(X1[:,0]+10), 200)
 y1_grid=np.linspace(np.min(X1[:,1]-10), np.max(X1[:,1]+10), 200)
-plot_vector_field(gp_deltaX1, x1_grid,y1_grid,X1,S1)
+plot_vector_field_minvar(gp_deltaX1, x1_grid,y1_grid,X1,S1)
 plt.show()
